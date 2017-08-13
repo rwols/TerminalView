66c1957dde09d594a6d94e1469be08a5b3ff32ca
Raoul Wols <raoulwols@gmail.com>
2017-08-13 13:41:25 +0200

    flake8 improvements

--- convert_color_scheme.py
+++ convert_color_scheme.py
@@ -30,14 +30,12 @@ def distance2(a, b):
 
 
 def next_color(color_text):
-    """
-    Given a color string "#xxxxxy", returns its next color "#xxxxx{y+1}".
-    """
+    """Given a color string "#xxxxxy", returns its next color "#xxxxx{y+1}"."""
     hex_value = int(color_text[1:], 16)
     if hex_value == 16777215:  # #ffffff
         return "#fffffe"
     else:
-        return "#{}".format(hex(hex_value+1)[2:])
+        return "#{}".format(hex(hex_value + 1)[2:])
 
 
 _name_from_index = ["black", "white", "red", "green", "blue", "brown", "magenta", "cyan"]
